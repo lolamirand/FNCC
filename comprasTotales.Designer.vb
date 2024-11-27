1c9d6271e2d3fc46b1d992fc8d6939509424fd4b
sole pastorutti <sala1pc06@hotmail.com>
2024-11-27 08:09:26 -0300

    prueba

--- comprasTotales.Designer.vb
+++ comprasTotales.Designer.vb
@@ -22,83 +22,83 @@ Partial Class comprasTotales
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
-        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.btnBuscar = New System.Windows.Forms.Button()
-        Me.Panel1.SuspendLayout()
-        Me.SuspendLayout()
-        '
-        'DateTimePicker1
-        '
-        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.DateTimePicker1.Location = New System.Drawing.Point(109, 31)
-        Me.DateTimePicker1.Name = "DateTimePicker1"
-        Me.DateTimePicker1.Size = New System.Drawing.Size(99, 23)
-        Me.DateTimePicker1.TabIndex = 0
-        '
-        'DateTimePicker2
-        '
-        Me.DateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.DateTimePicker2.Location = New System.Drawing.Point(382, 31)
-        Me.DateTimePicker2.Name = "DateTimePicker2"
-        Me.DateTimePicker2.Size = New System.Drawing.Size(99, 23)
-        Me.DateTimePicker2.TabIndex = 1
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(27, 37)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(76, 15)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Fecha Desde:"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(302, 37)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(74, 15)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "Fecha Hasta:"
-        '
-        'Panel1
-        '
-        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Panel1.Controls.Add(Me.btnBuscar)
-        Me.Panel1.Controls.Add(Me.Label2)
-        Me.Panel1.Controls.Add(Me.DateTimePicker1)
-        Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Controls.Add(Me.DateTimePicker2)
-        Me.Panel1.Location = New System.Drawing.Point(59, 28)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(660, 100)
-        Me.Panel1.TabIndex = 4
-        '
-        'btnBuscar
-        '
-        Me.btnBuscar.Location = New System.Drawing.Point(553, 37)
-        Me.btnBuscar.Name = "btnBuscar"
-        Me.btnBuscar.Size = New System.Drawing.Size(75, 23)
-        Me.btnBuscar.TabIndex = 4
-        Me.btnBuscar.Text = "Buscar"
-        Me.btnBuscar.UseVisualStyleBackColor = True
-        '
-        'comprasTotales
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Panel1)
-        Me.Name = "comprasTotales"
-        Me.Text = "comprasTotales"
-        Me.Panel1.ResumeLayout(False)
-        Me.Panel1.PerformLayout()
-        Me.ResumeLayout(False)
-
+        DateTimePicker1 = New DateTimePicker()
+        DateTimePicker2 = New DateTimePicker()
+        Label1 = New Label()
+        Label2 = New Label()
+        Panel1 = New Panel()
+        btnBuscar = New Button()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Format = DateTimePickerFormat.Short
+        DateTimePicker1.Location = New Point(109, 31)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(99, 23)
+        DateTimePicker1.TabIndex = 0
+        ' 
+        ' DateTimePicker2
+        ' 
+        DateTimePicker2.Format = DateTimePickerFormat.Short
+        DateTimePicker2.Location = New Point(382, 31)
+        DateTimePicker2.Name = "DateTimePicker2"
+        DateTimePicker2.Size = New Size(99, 23)
+        DateTimePicker2.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(27, 39)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(76, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "Fecha Desde:"' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(302, 39)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(74, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "Fecha Hasta:"' 
+        ' Panel1
+        ' 
+        Panel1.BorderStyle = BorderStyle.Fixed3D
+        Panel1.Controls.Add(btnBuscar)
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(DateTimePicker1)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(DateTimePicker2)
+        Panel1.Location = New Point(59, 28)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(660, 100)
+        Panel1.TabIndex = 4
+        ' 
+        ' btnBuscar
+        ' 
+        btnBuscar.BackColor = Color.MidnightBlue
+        btnBuscar.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        btnBuscar.ForeColor = SystemColors.ControlLightLight
+        btnBuscar.Location = New Point(552, 31)
+        btnBuscar.Name = "btnBuscar"
+        btnBuscar.Size = New Size(67, 23)
+        btnBuscar.TabIndex = 4
+        btnBuscar.Text = "Buscar"
+        btnBuscar.UseVisualStyleBackColor = False
+        ' 
+        ' comprasTotales
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Panel1)
+        Name = "comprasTotales"
+        Text = "comprasTotales"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        ResumeLayout(False)
     End Sub
 
     Friend WithEvents DateTimePicker1 As DateTimePicker
